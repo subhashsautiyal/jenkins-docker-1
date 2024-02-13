95b252ba8e17edfd6fa2c196448fac47a7f609f8
subhashsautiyal <68697760+subhashsautiyal@users.noreply.github.com>
2024-02-13 15:35:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>  Version=0LOCAL.
+          Edit <code>src/App.js</code>  Version=PANI.
         </p>
         <a
           className="App-link"
